a4921c0dd5617e0ae16bda7dbce12a88b47914e6
Prince Kanaugia <princekanaugia@gmail.com>
2025-03-07 14:51:06 +0530

    Added header hero img and styles for main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import Search from './components/Search'
 
 function App() {
 
-  return <>
-    <div > hello MovieDB</div>
-  </>
+  return <main>
+    <div className='Patttern' />
+
+    <div className='wrapper'>
+      <header>
+        <img src='./hero-img.png' alt='Hero Banner' />
+        <h1>
+          Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hassel
+        </h1>
+      </header>
+
+      <Search />
+    </div>
+  </main>
 }
 
 export default App
---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div>Searcs</div>
+  )
+}
+
+export default Search
